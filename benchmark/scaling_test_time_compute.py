92111ccf866a8e83f5b8b7ab1ea7b32477f87044
WALL-AI <gaojing850063636@gmail.com>
2025-02-27 18:37:31 +0800

    feat:add test time compute

---
+++ benchmark/scaling_test_time_compute.py
@@ -0,0 +1,7 @@
+##Scaling Test-Time Compute https://huggingface.co/learn/cookbook/en/search_and_learn
+##https://github.com/RLHFlow/RLHF-Reward-Modeling/tree/main/math-rm
+##https://huggingface.co/RLHFlow/Llama3.1-8B-PRM-Deepseek-Data  
+'''
+评测一下test time的时间
+
+'''
